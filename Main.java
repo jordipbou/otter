5b06d9c3e43ee87a3a46dc84083bc00aec766e14
jordipbou <jordipbou@protonmail.com>
2023-11-02 22:34:25 +0000

    Syncing.

--- Main.java
+++ Main.java
@@ -2,17 +2,32 @@ import java.io.*;
 
 class Main {
   public static void main(String[] args) throws IOException {
-    BufferedReader obj = 
-      new BufferedReader(
-        new InputStreamReader(
-          System.in)); 
+    
     Otter o = new Otter();
     Sloth s = new Sloth(o, 65536);
     
     // o.extensions['H' - 'A'] = x -> { System.out.println("Hello world!"); };
     o.extensions['C' - 'A'] = new Combinators();
     o.extensions['S' - 'A'] = s;
-  
+
+    BufferedReader f =
+      new BufferedReader(
+        new InputStreamReader(
+          new FileInputStream("ans.fth")));
+
+    try {
+    while (true) {
+      String l = f.readLine();
+      if (l == null) break;
+      s.evaluate(o, l);
+    }
+    } catch(IOException e) {}
+    
+      BufferedReader obj = 
+        new BufferedReader(
+          new InputStreamReader(
+            System.in)); 
+    
     while (true) {
       System.out.print("> ");
       //o.isolated(obj.readLine());
